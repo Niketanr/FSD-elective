a07ab217feb964ebaf274eb2fb85f4082b20bb7b
Niketan <amrita1234@gmail.com>
2025-12-29 11:18:40 +0530

    calculator code in master branch by user 1

---
+++ calc.py
@@ -0,0 +1,16 @@
+^[[200~# Simple Calculator Project
+
+def add(x,y):
+return x+y
+
+def subtract(x,y):
+pass
+
+def multiply(x,y):
+pass
+
+def divide(x,y):
+pass
+
+if__name__==__main__:
+print(Calculator started.)_
